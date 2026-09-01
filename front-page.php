0a606ce0483af5edc4b9eb1ea366337eba93cde8
Mahdimosharafi <mahdimosharafii@gmail.com>
2026-09-02 00:19:50 +0330

    Connect homepage navigation to articles and notices

--- front-page.php
+++ front-page.php
@@ -9,14 +9,21 @@ $homepage = ob_get_clean();
 $properties_url = get_post_type_archive_link('property') ?: home_url('/properties/');
 $areas_url = get_post_type_archive_link('melkino_area') ?: home_url('/areas/');
 $agents_url = get_post_type_archive_link('agent') ?: home_url('/agents/');
+$articles_url = get_post_type_archive_link('melkino_article') ?: home_url('/articles/');
+$notices_url = get_post_type_archive_link('melkino_notice') ?: home_url('/notices/');
 
 $homepage = str_replace('href="#properties"', 'href="' . esc_url($properties_url) . '"', $homepage);
 $homepage = str_replace('href="#all-properties"', 'href="' . esc_url($properties_url) . '"', $homepage);
 $homepage = str_replace('href="#areas"', 'href="' . esc_url($areas_url) . '"', $homepage);
 $homepage = str_replace('href="#consultants"', 'href="' . esc_url($agents_url) . '"', $homepage);
+$homepage = str_replace('href="#articles"', 'href="' . esc_url($articles_url) . '"', $homepage);
+$homepage = str_replace('href="#notices"', 'href="' . esc_url($notices_url) . '"', $homepage);
+$homepage = str_replace('href="#all-articles"', 'href="' . esc_url($articles_url) . '"', $homepage);
+$homepage = str_replace('href="#all-notices"', 'href="' . esc_url($notices_url) . '"', $homepage);
 
-// Convert any remaining internal section links to real archive pages where appropriate.
 $homepage = str_replace('href="/areas/"', 'href="' . esc_url($areas_url) . '"', $homepage);
 $homepage = str_replace('href="/agents/"', 'href="' . esc_url($agents_url) . '"', $homepage);
+$homepage = str_replace('href="/articles/"', 'href="' . esc_url($articles_url) . '"', $homepage);
+$homepage = str_replace('href="/notices/"', 'href="' . esc_url($notices_url) . '"', $homepage);
 
 echo $homepage;
