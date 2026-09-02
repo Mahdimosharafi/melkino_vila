20fd18f0e3f6b7efe1734c33bbbb9334f5b423a4
Mahdimosharafi <mahdimosharafii@gmail.com>
2026-09-02 11:04:54 +0330

    Connect homepage login and property submission buttons

--- front-page.php
+++ front-page.php
@@ -1,17 +1,17 @@
 <?php
 /** Front Page Template: Melkino Vila */
 if (!defined('ABSPATH')) exit;
-
 ob_start();
 require get_template_directory() . '/index.php';
 $homepage = ob_get_clean();
-
 $properties_url = get_post_type_archive_link('property') ?: home_url('/properties/');
 $areas_url = get_post_type_archive_link('melkino_area') ?: home_url('/areas/');
 $agents_url = get_post_type_archive_link('agent') ?: home_url('/agents/');
 $articles_url = get_post_type_archive_link('melkino_article') ?: home_url('/articles/');
 $notices_url = get_post_type_archive_link('melkino_notice') ?: home_url('/notices/');
 $submit_property_url = home_url('/submit-property/');
+$login_url = home_url('/login/');
+$account_url = home_url('/account/');
 $about_page = get_page_by_path('about');
 $contact_page = get_page_by_path('contact');
 $about_url = $about_page ? get_permalink($about_page) : home_url('/about/');
@@ -25,17 +25,16 @@ $homepage = str_replace('href="#articles"', 'href="' . esc_url($articles_url) . 
 $homepage = str_replace('href="#notices"', 'href="' . esc_url($notices_url) . '"', $homepage);
 $homepage = str_replace('href="#all-articles"', 'href="' . esc_url($articles_url) . '"', $homepage);
 $homepage = str_replace('href="#all-notices"', 'href="' . esc_url($notices_url) . '"', $homepage);
-
 $homepage = str_replace('href="/areas/"', 'href="' . esc_url($areas_url) . '"', $homepage);
 $homepage = str_replace('href="/agents/"', 'href="' . esc_url($agents_url) . '"', $homepage);
 $homepage = str_replace('href="/articles/"', 'href="' . esc_url($articles_url) . '"', $homepage);
 $homepage = str_replace('href="/notices/"', 'href="' . esc_url($notices_url) . '"', $homepage);
 $homepage = str_replace('href="#sell"', 'href="' . esc_url($submit_property_url) . '"', $homepage);
 $homepage = str_replace('href="/submit-property/"', 'href="' . esc_url($submit_property_url) . '"', $homepage);
+$homepage = str_replace('href="#register-property"', 'href="' . esc_url($submit_property_url) . '"', $homepage);
+$homepage = str_replace('href="#login"', 'href="' . esc_url(is_user_logged_in() ? $account_url : $login_url) . '"', $homepage);
 $homepage = str_replace('href="#about"', 'href="' . esc_url($about_url) . '"', $homepage);
 $homepage = str_replace('href="#contact"', 'href="' . esc_url($contact_url) . '"', $homepage);
-
-// Make any remaining submit-property style links point to the real submission page.
+if(is_user_logged_in()) $homepage = str_replace('ورود / ثبت‌نام','حساب کاربری',$homepage);
 $homepage = preg_replace('/href=["\']#(?:submit-property|submit)["\']/i', 'href="' . esc_url($submit_property_url) . '"', $homepage);
-
 echo $homepage;
